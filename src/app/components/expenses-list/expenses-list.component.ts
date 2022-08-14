b8b003f88d85087a47439d29e762b9baea934c9e
dCurre <davidecurreli@gmail.com>
2022-08-15 00:14:04 +0200

    Modifica home lista spese

--- src/app/components/expenses-list/expenses-list.component.ts
+++ src/app/components/expenses-list/expenses-list.component.ts
@@ -4,6 +4,9 @@ import { ExpensesListService } from '../../shared/services/firestore/expensesLis
 import { ExpensesList } from '../../shared/services/firestore/expensesList/expenses-list';
 import { Observable } from 'rxjs';
 import { ExpensesListFieldsEnum } from 'src/app/shared/enums/expensesListFieldsEnum';
+import { UserService } from 'src/app/shared/services/firestore/user/user.service';
+import { UserFieldsEnum } from 'src/app/shared/enums/userFieldsEnum';
+import { User } from 'src/app/shared/services/firestore/user/user';
 
 @Component({
   selector: 'app-expenses-list',
@@ -13,21 +16,34 @@ import { ExpensesListFieldsEnum } from 'src/app/shared/enums/expensesListFieldsE
 export class ExpensesListComponent implements OnInit {
 
   protected expensesLists$: Observable<ExpensesList[]>;
+  protected listOwner$: Observable<User>;
 
   constructor(
     public afAuth: AngularFireAuth,
-    private expensesListService: ExpensesListService) { }
+    private expensesListService: ExpensesListService,
+    private userService: UserService) { }
 
   ngOnInit(): void {
     this.getExpensesListsByLoggedUser()
+    this.getListOwner()
   }
 
   async getExpensesListsByLoggedUser() {
     this.afAuth.authState.subscribe(user => {
-      try{
+      try {
         this.expensesLists$ = this.expensesListService.getExpensesListsByUserId(ExpensesListFieldsEnum.PARTECIPANTS, user!!.uid);
-      } catch (e){
-          console.error("HomeComponent.getLoggedUser: ", e)
+      } catch (e) {
+        console.error("HomeComponent.getLoggedUser: ", e)
+      }
+    });
+  }
+
+  async getListOwner() {
+    this.afAuth.authState.subscribe(user => {
+      try {
+        this.listOwner$ = this.userService.getUserByField(UserFieldsEnum.ID, user!!.uid);
+      } catch (e) {
+        console.error("HomeComponent.getLoggedUser: ", e)
       }
     });
   }
